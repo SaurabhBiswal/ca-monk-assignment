1a0bfacd48f7344d387d8b757b98230a39380a6b
SaurabhBiswal <punpunsaurabh2002@gmail.com>
2026-01-20 00:36:14 +0530

    fix: standardize API paths and baseURL for better reliability

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import type { Blog, CreateBlogInput } from '../types/blog.ts';
 
-const API_BASE_URL = 'https://my-json-server.typicode.com/SaurabhBiswal/ca-monk-assignment';
+const API_BASE_URL = 'https://my-json-server.typicode.com/SaurabhBiswal/ca-monk-assignment/';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
@@ -9,15 +9,15 @@ const api = axios.create({
 
 export const blogService = {
     getAllBlogs: async (): Promise<Blog[]> => {
-        const response = await api.get('/blogs');
+        const response = await api.get('blogs');
         return response.data;
     },
     getBlogById: async (id: string): Promise<Blog> => {
-        const response = await api.get(`/blogs/${id}`);
+        const response = await api.get(`blogs/${id}`);
         return response.data;
     },
     createBlog: async (blog: CreateBlogInput): Promise<Blog> => {
-        const response = await api.post('/blogs', blog);
+        const response = await api.post('blogs', blog);
         return response.data;
     },
 };
